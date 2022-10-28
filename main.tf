825886280e91bd1e45dad4243042394de1c35127
root <root@workspace.e00049.com>
2022-10-28 17:14:32 +0000

    002 Custom VPC

--- main.tf
+++ main.tf
@@ -16,3 +16,18 @@ resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
   location = var.location
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  address_space       = ["192.168.1.0/24"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["192.168.1.0/26"]
+}
